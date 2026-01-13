f4173d16e1586fc0b7247425ffd81f7e9c819109
Javier Alonso Caicedo Tobon <javiercaicedotobon@gmail.com>
2026-01-13 11:32:04 -0500

    Se desarrolla lo referente a admin.py del modelo producto

--- portafolio/applications/producto/admin.py
+++ portafolio/applications/producto/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
-
+#
+from .models import Servicios, Proyecto, Habilidades
 # Register your models here.
+
+
+class ServiciosAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'titulo',
+        'tipo_servicio',
+        'descripcion',
+        'icon',
+        'is_active'
+    )
+
+
+admin.site.register(Servicios, ServiciosAdmin)
+
+
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'titulo',
+        'descripcion',
+        'tecnologias',
+        'imagen',
+        'url'
+    )
+
+
+admin.site.register(Proyecto, ProyectoAdmin)
+
+
+
+class HabilidadesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+        'nivel'
+    )
+
+
+admin.site.register(Habilidades, HabilidadesAdmin)
